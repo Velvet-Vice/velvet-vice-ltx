74c1bc093df0e900ca7e03e0be7660cc2b018068
Velvet Vice <daagle42@googlemail.com>
2026-09-05 12:14:07 +0200

    Canonicalize LTX package identity for Manager uninstall

--- version.py
+++ version.py
@@ -1,3 +1,9 @@
-PACK_VERSION = "1.2.7"
-PACK_NAME = "ComfyUI-Velvet-Vice-LTX"
+PACK_VERSION = "1.2.8"
+PACK_NAME = "velvet-vice-ltx"
+REGISTRY_NODE_ID = "velvet-vice-ltx"
+LEGACY_PACK_NAMES = (
+    "ComfyUI-Velvet-Vice-LTX",
+    "ComfyUI-Velvet-Vice-LTX-main",
+    "velvet-vice-ltx-main",
+)
 SUPPORTED_LTX_VERSIONS = ("2.3", "2.5")
